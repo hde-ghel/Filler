2db987acb942b0721e6c3a5e6740f05038456c3b
hde-ghel <hde-ghel@student.s19.be>
2020-02-06 11:53:43 +0100

    clean repo

--- include/filler.h
+++ include/filler.h
@@ -6,8 +6,29 @@
 /*   By: hde-ghel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 13:46:05 by hde-ghel          #+#    #+#             */
-/*   Updated: 2020/01/24 13:46:46 by hde-ghel         ###   ########.fr       */
+/*   Updated: 2020/02/03 18:40:09 by hde-ghel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FILLER_H
+# define FILLER_H
+
 # include "../libft/libft.h"
+
+typedef	struct	s_filler
+{
+	char	**map;
+	char	**piece;
+	int		map_x;
+	int		map_y;
+	int		piece_x;
+	int		piece_y;
+	int		player;
+
+}				t_filler;
+
+
+
+
+
+#endif
