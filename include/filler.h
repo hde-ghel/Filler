edabd675c2c301a4bd3404893831e41c19170dc6
hde-ghel <hde-ghel@student.s19.be>
2020-02-06 13:04:21 +0100

    added log file

--- include/filler.h
+++ include/filler.h
@@ -6,7 +6,7 @@
 /*   By: hde-ghel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 13:46:05 by hde-ghel          #+#    #+#             */
-/*   Updated: 2020/02/03 18:40:09 by hde-ghel         ###   ########.fr       */
+/*   Updated: 2020/02/06 13:03:52 by hde-ghel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,22 @@
 # define FILLER_H
 
 # include "../libft/libft.h"
+# include <fcntl.h>
+
+# include <stdio.h>
 
 typedef	struct	s_filler
 {
 	char	**map;
 	char	**piece;
-	int		map_x;
-	int		map_y;
-	int		piece_x;
-	int		piece_y;
+	int		msize_x;
+	int		msize_y;
+	int		psize_x;
+	int		psize_y;
 	int		player;
 
+
+	FILE		*fd_log;
 }				t_filler;
 
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hde-ghel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 18:40:39 by hde-ghel          #+#    #+#             */
-/*   Updated: 2020/02/03 18:52:03 by hde-ghel         ###   ########.fr       */
+/*   Updated: 2020/02/06 13:03:48 by hde-ghel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,32 @@
 
 void		check_player(t_filler *env)
 {
+	env->msize_x = 2;
 
+}
+
+void		first_run(t_filler *env)
+{
+	check_player(env);
+	env->msize_x = 2;
 }
 
 int	main(void)
 {
 	t_filler	env;
 
-	check_player(&env);
-
+	ft_bzero(&env, sizeof(t_filler));
+	if ((env.fd_log = fopen("filler_log", "w")) < 0)
+		return (-1);
+	first_run(&env);
+	/*
 	while (42)
 	{
 
 	}
+	*/
+	fprintf(env.fd_log, "%s", "boudin\n");
+	fprintf(env.fd_log, "%s", "boudin dfhdsfgsd\n");
+	fprintf(env.fd_log, "%s", "boudindfgj \n");
+	return (0);
 }
